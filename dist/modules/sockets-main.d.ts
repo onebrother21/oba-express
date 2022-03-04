d6d5ed62d21a424c089c2b3f4936711bcf479075
onebrother21 <service.onebrother@gmail.com>
2022-03-04 13:16:07 -0600

    rebuilding with major changes

--- dist/modules/sockets-main.d.ts
+++ dist/modules/sockets-main.d.ts
@@ -3,6 +3,6 @@ import { Server } from "http";
 import { OBAExpressApiSocketsConfigType } from "./sockets-types";
 export declare type OBAExpressApiSocketsConfig<Sockets> = OBAExpressApiSocketsConfigType<Sockets>;
 export declare class OBAExpressApiSockets {
-    static init: <Sockets>(config: OBAExpressApiSocketsConfig<Sockets>, httpServer: Server) => socketIo.Server<import("socket.io/dist/typed-events").DefaultEventsMap, import("socket.io/dist/typed-events").DefaultEventsMap, import("socket.io/dist/typed-events").DefaultEventsMap>;
+    static init: <Sockets>(config: OBAExpressApiSocketsConfig<Sockets>, httpServer: Server) => socketIo.Server<import("socket.io/dist/typed-events").DefaultEventsMap, import("socket.io/dist/typed-events").DefaultEventsMap, import("socket.io/dist/typed-events").DefaultEventsMap, any>;
 }
 export default OBAExpressApiSockets;
