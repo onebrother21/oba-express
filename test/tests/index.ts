6c4d38ff0ba722abe03fda1870ca71670633cef4
onebrother21 <service.onebrother@gmail.com>
2022-08-13 01:17:42 -0500

    rebuilding with minor changes

--- test/tests/index.ts
+++ test/tests/index.ts
@@ -17,7 +17,7 @@ export const finalCheck = () => J.desc("INIT EXPRESS API",() => {
   },1E9);
 });
 export const wrapup = () => J.desc("WRAPUP",() => {
-  it("Wrapup",async () => {OB.log("complete");},1E9);
+  it("Wrapup",async () => {OB.log("**test complete**");},1E9);
 });
 export const allTests = () => J.desc("ALL TESTS",() => {
   init();
